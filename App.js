9c49f8ad01f45b820f3ff79b6bd79bc244295590
palevoo <kud.nikita@gmail.com>
2018-07-27 14:22:49 +0300

    added faker to generate fake data

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import faker from "faker";
 
 export default class App extends React.Component {
+  state = {
+    email: ""
+  };
+
+  componentDidMount() {
+    let email = faker.internet.email();
+    console.log("HERE", this.state);
+
+    this.setState({
+      email: email
+    });
+    console.log("HERE", this.state);
+  }
   render() {
     return (
       <View style={styles.container}>
+        <Text>Email: {this.state.email}</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -16,8 +31,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
