9611dbe122c58f3190c062adb8d67aa8dabf21c0
palevoo <kud.nikita@gmail.com>
2018-07-31 00:36:57 +0300

    added filter by cat and id, mvp styling and layout

--- components/SlideMe.js
+++ components/SlideMe.js
@@ -1,36 +1,73 @@
 /* @flow */
 
 import React, { Component } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList, Dimensions } from "react-native";
 
 import FoodItem from "./FoodItem";
+import NavBar from "./NavBar";
+import Paginate from "./Paginate";
+
+const { width, height } = Dimensions.get("window");
+const DEFAULT_CAT = "Breakfast";
 
 export default class SlideMe extends Component {
+  state = {
+    activeCategory: "",
+    data: this.props.data,
+    filteredByCategory: []
+  };
+  componentDidMount() {
+    this.setState(() => ({
+      activeCategory: DEFAULT_CAT,
+      filteredByCategory: this._filterByCategory(this.state.data, DEFAULT_CAT)
+    }));
+  }
   render() {
     return (
       <View style={styles.container}>
+        <NavBar handleCategory={this._handleCategory} />
+        <Paginate
+          items={this.state.filteredByCategory}
+          handleItem={this._handleItem}
+        />
         <FlatList
           horizontal
           showsHorizontalScrollIndicator={false}
-          centerContent
           pagingEnabled
-          style={styles.container}
-          data={this.props.data}
-          extraData={this.state}
+          // style={styles.container}
+          data={this.state.filteredByCategory}
+          // ref={r => (this.refs = r)}
           keyExtractor={this._keyExtractor}
+          // onViewableItemsChanged={this.onViewableItemsChanged} // Map your keys to whatever unique ids the have (mine is a "id" prop)
           renderItem={this._renderItem}
         />
       </View>
     );
   }
 
+  _filterByCategory = (data, category) => {
+    return data.filter(el => el.category === category);
+  };
+  _handleCategory = category => {
+    this.setState(() => ({
+      activeCategory: category,
+      filteredByCategory: this._filterByCategory(this.state.data, category)
+    }));
+  };
+  _handleItem = item => {
+    console.log("PARENTO", item);
+  };
+  _keyExtractor = (item: Object, index: Number) => {
+    return item.id;
+  };
   _logger = (id: String) => {
     console.log(id);
   };
   _renderItem({ item }) {
     return (
       <FoodItem
-        key={item.id}
+        index={item.id}
+        category={item.category}
         color={item.color}
         id={item.id}
         imageUrl={item.image}
@@ -43,5 +80,9 @@ export default class SlideMe extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    width,
+    height: height / 2
+  }
 });
