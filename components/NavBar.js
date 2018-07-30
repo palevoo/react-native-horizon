f36cf3e3e102d7f24671a78a6c7ce458f1fdcd7f
palevoo <kud.nikita@gmail.com>
2018-07-31 00:37:25 +0300

    added mvp navbar to navigate by categories

---
+++ components/NavBar.js
@@ -0,0 +1,64 @@
+/* @flow */
+
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Dimensions
+} from "react-native";
+import { CATEGORIES } from "../utils";
+
+export default class NavBar extends Component {
+  state = {
+    categories: CATEGORIES
+  };
+
+  renderMenuItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        item={item}
+        onPress={() => this.props.handleCategory(item)}
+      >
+        <View style={styles.menuItem}>
+          <Text style={styles.menuText}>{item}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    const { handleCategory } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={this.state.categories}
+          renderItem={this.renderMenuItem}
+          keyExtractor={item => item}
+          handleCategory={handleCategory}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    width: Dimensions.get("window").width,
+    backgroundColor: "#35D788"
+  },
+  menuItem: {
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  menuText: {
+    fontSize: 24,
+    color: "#fafafa"
+  }
+});
